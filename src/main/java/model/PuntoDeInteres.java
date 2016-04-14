1b232876fe4945eb9835cb6f6451764599dfcbd3
HaydossianEzequiel <ezeqhay@hotmail.com>
2016-04-13 23:04:44 -0300

    Se crea la interface Punto de interes y se crean las clases que deben implementarla

--- src/main/java/model/PuntoDeInteres.java
+++ src/main/java/model/PuntoDeInteres.java
@@ -1,44 +1,14 @@
 package model;
 
-public class PuntoDeInteres {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
-	private String calle;
-
-	private String altura;
-	private int cuadrasDeCercania;
-	private DisponibilidadHoraria disponibilidadHoraria;
-
-	public boolean estaDisponible(Object date) {
-		return disponibilidadHoraria.incluye(date);
-	}
-
-	public boolean estaCerca(int x, int y)
-	{
-		return true;
-	}
-	public boolean estaCerca(PuntoDeInteres poi) {
-		return DistanciaEnCuadras(calle, altura, poi.getCalle(), poi.getAltura()) < cuadrasDeCercania;
-	}
-
-	private int DistanciaEnCuadras(String calle1, String altura1, String calle2, String altura2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public boolean estaDelimitadoPor(Object radio) {
-		return true;
-	}
-
-	public String getCalle() {
-		return calle;
-	}
-
-	public String getAltura() {
-		return altura;
-	}
-
-	public int getCallesDeCercania() {
-		return cuadrasDeCercania;
-	}
-
+public interface PuntoDeInteres {
+	
+	
+	public boolean estaDisponible(LocalDate date);
+	
+	public boolean estaCerca(int x, int y) ;
+	
+	public ArrayList<PuntoDeInteres> buscar(String textoLibre);
 }
---
+++ src/main/java/model/SucursalDeBanco.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class SucursalDeBanco implements PuntoDeInteres {
+
+	private String calle;
+	private String altura;
+	private int cuadrasDeCercania;
+	private int x;
+	private int y;
+	
+	@Override
+	public boolean estaDisponible(LocalDate date) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estaCerca(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<PuntoDeInteres> buscar(String textoLibre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
