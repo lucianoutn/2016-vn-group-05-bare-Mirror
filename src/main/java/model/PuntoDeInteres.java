c6786393b365f27b25a2fd4b7e7d838242eb88cb
Luciano Marques <luciano.utn@gmail.com>
2016-04-15 15:30:43 -0300

    formato

--- src/main/java/model/PuntoDeInteres.java
+++ src/main/java/model/PuntoDeInteres.java
@@ -4,14 +4,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface PuntoDeInteres {
-	
-	
+
 	public boolean estaDisponible(LocalDate date);
-	
-	public boolean estaCerca(int x, int y) ;
-	
-}
-	public boolean encuentra(String textoLibre); 
-	//A partir de un texto libre, busco en todas los atributos si el objeto aplica al texto de busqueda
-	//Si alguno aplica, devuelvo true
+
+	public boolean estaCerca(int x, int y);
+
+	public boolean encuentra(String textoLibre);
+	// A partir de un texto libre, busco en todas los atributos si el objeto
+	// aplica al texto de busqueda
+	// Si alguno aplica, devuelvo true
 }
