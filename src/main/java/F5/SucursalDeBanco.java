7505d26b7261df35ee2ae3eeca65c9b44225879b
Luciano Marques <luciano.utn@gmail.com>
2016-04-19 03:55:00 -0300

    Subo codigo de Alejandro (esta sin luz) :(

--- src/main/java/F5/SucursalDeBanco.java
+++ src/main/java/F5/SucursalDeBanco.java
@@ -6,30 +6,55 @@ import java.util.Collection;
 //import F5.Sucursal;
 
 import org.uqbar.geodds.Point;
+import org.uqbar.geodds.Polygon;
 
 public class SucursalDeBanco implements PuntoDeInteres {
 
 	private String calle;
 	private int altura;
-	private int x;//Asumimos que las coordenadas son cuadras
+	private int x;// Asumimos que las coordenadas son cuadras
 	private int y;
 	private String unNombreDeBanco;
-	private Collection<Sucursal> sucursales;
-	private Sucursal unaSucursal;
-	
-	/* El POI de una Sucursal de Banco se encuentra cerca si, desde el punto actual esta a 5 cuadras */
+	private Collection<SucursalDeBanco> sucursales;
+	private LocalDate horarioInicial;
+	private LocalDate horarioCierre;
+	private Collection<String> diasDeAtencion;
+
+	/*
+	 * El POI de una Sucursal de Banco se encuentra cerca si, desde el punto
+	 * actual esta a 5 cuadras
+	 */
 	@Override
 	public boolean estaCerca(Point point) {
-		int distancia = Math.abs(x-this.x) + Math.abs(y-this.y);
+		int distancia = Math.abs(x - this.x) + Math.abs(y - this.y);
 		return (distancia < 5);
 	}
-	
-	public boolean estaDisponible(LocalDate horaActual){
-		// TODO
-		 unaSucursal = null;
+
+	// private int Partidax;
+	// private int Partiday;
+	// arrayList<int> puntoDePartida = new arrayList<int>();
+	// puntoDePartida.add(Partidax); //Determinar como obtener la posicion
+	// actual
+	// puntoDePartida.add(Partiday);
+	// arrayList<int> puntoDeLlegada = new arrayList<int>();
+	// puntoDePartida.add(Llegadax); //Determinar como obtener la posicion
+	// actual
+	// puntoDePartida.add(Llegaday);
+	// public boolean estaCerca(arrayList<int> puntoDePartida, arrayList<int>
+	// puntoDeLlegada){
+	// }
+
+	public boolean estaDisponible(LocalDate horaActual) {
+		SucursalDeBanco unaSucursal = null;
 		if (sucursales.contains(unaSucursal) && unaSucursal.estaAbierto(horaActual))
 			return true;
+		return false;
+	}
 
+	public boolean estaAbierto(LocalDate horaActual) {
+		if (horaActual.isAfter(horarioInicial) && horaActual.isBefore(horarioCierre)) {
+			return true;
+		}
 		return false;
 	}
 
@@ -43,6 +68,5 @@ public class SucursalDeBanco implements PuntoDeInteres {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
 
 }
