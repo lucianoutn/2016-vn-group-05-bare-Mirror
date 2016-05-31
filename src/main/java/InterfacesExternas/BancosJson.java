e9de003d1762437ccfb1e941cf2b3bfb9856e81a
Luciano Marques <luciano.utn@gmail.com>
2016-05-31 00:23:58 -0300

    test del parser de json

--- src/main/java/InterfacesExternas/BancosJson.java
+++ src/main/java/InterfacesExternas/BancosJson.java
@@ -1,50 +1,85 @@
 package InterfacesExternas;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BancosJson {
-	
+
+	@JsonProperty("banco")
 	private String nombre;
+
+	@JsonProperty("x")
 	private String x;
+
+	@JsonProperty("y")
 	private String y;
+
+	@JsonProperty("sucursal")
 	private String sucursal;
+
+	@JsonProperty("gerente")
 	private String gerente;
-	private String servicios;
-	
+
+	@JsonProperty("servicios")
+	private List<String> servicios;
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getX() {
 		return x;
 	}
+
 	public void setX(String x) {
 		this.x = x;
 	}
+
 	public String getY() {
 		return y;
 	}
+
 	public void setY(String y) {
 		this.y = y;
 	}
+
 	public String getSucursal() {
 		return sucursal;
 	}
+
 	public void setSucursal(String sucursal) {
 		this.sucursal = sucursal;
 	}
+
 	public String getGerente() {
 		return gerente;
 	}
+
 	public void setGerente(String gerente) {
 		this.gerente = gerente;
 	}
-	public String getServicios() {
+
+	public List<String> getServicios() {
 		return servicios;
 	}
-	public void setServicios(String servicios) {
-		this.servicios = servicios;
+
+	public void setServicios(List<String> lista) {
+
+		this.servicios.addAll(lista);// = lista;
+
+	}
+
+	public void setServicios(String e) {
+		this.servicios.add(e);
+		
 	}
 	
+	//public void setServicios()
 
 }
