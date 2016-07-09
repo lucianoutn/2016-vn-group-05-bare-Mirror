583a13ada5bb51d30c8e446f49896092614fa6a7
Luciano Marques <luciano.utn@gmail.com>
2016-07-09 00:22:21 -0300

    enviador de Emails

---
+++ src/main/java/F5/EmailUtil.java
@@ -0,0 +1,35 @@
+package F5;
+
+import java.util.Date;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class EmailUtil {
+
+	
+	public static void sendEmail(Session session, String toEmail, String subject, String body){
+		try
+	    {
+	      MimeMessage msg = new MimeMessage(session);
+	      msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+	      msg.addHeader("format", "flowed");
+	      msg.addHeader("Content-Transfer-Encoding", "8bit");
+	      msg.setFrom(new InternetAddress("noresponder@grupoF5.com.ar", "noresponder"));
+	      msg.setReplyTo(InternetAddress.parse("noresponder@grupoF5.com.ar", false));
+	      msg.setSubject(subject, "UTF-8");
+	      msg.setText(body, "UTF-8");
+	      msg.setSentDate(new Date());
+	      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+	      System.out.println("Mensaje Listo");
+    	  Transport.send(msg);  
+	      System.out.println("Mail enviado correctamente!!");
+	    }
+	    catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	}
+}
