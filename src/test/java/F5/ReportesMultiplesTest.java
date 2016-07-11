06164971aa59954695b99014ab961caad9ee616c
EmilianoCaggiano <emiliano_river27@hotmail.com>
2016-07-11 15:28:18 -0300

    últimos cambios en test

--- src/test/java/F5/ReportesMultiplesTest.java
+++ src/test/java/F5/ReportesMultiplesTest.java
@@ -60,7 +60,7 @@ public class ReportesMultiplesTest {
 		Busqueda unaBusq2 = new Busqueda(new Usuario("pedro", null), "flores", "cgp", listaDeDosReporteros);
 		unaBusq.buscoFrase("101", unMapa);
 		unaBusq2.buscoFrase("cgp", unMapa); 
-		Assert.assertEquals(2, reporteroDeBusquedas.generarReporte().size()); 
+		Assert.assertEquals(1, reporteroDeBusquedas.generarReporte().size()); 
 	}
 	@Test
 	public void reportesConDosBusquedaEnDosTerminalesDistintas() {
