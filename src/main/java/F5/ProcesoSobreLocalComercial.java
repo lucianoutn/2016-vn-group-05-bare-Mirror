ddb1f245d6773e5bb93e11c00444db82a1ca8149
EmilianoCaggiano <emiliano_river27@hotmail.com>
2016-07-27 16:49:50 -0300

    Aislo lógica

--- src/main/java/F5/ProcesoSobreLocalComercial.java
+++ src/main/java/F5/ProcesoSobreLocalComercial.java
@@ -15,12 +15,14 @@ import F5.Proceso;
 public class ProcesoSobreLocalComercial extends ProcesoSobreLocalComercialClaseAbst {
 
 	private File archivo;
+	private lectorArchivoLocalComercial traductor;
 	
 	public ProcesoSobreLocalComercial(LocalDate horarioDeEjecucion, String rutaArchivo, RepositorioDePOIs map, int horaPlanificacion) {
 		this.setRuta(rutaArchivo);
 		setUnMapa(map);
 		this.setFecha(horarioDeEjecucion);
 		horarioDeEjecucion = horarioDeEjecucion;
+		traductor= new lectorArchivoLocalComercial();
 		this.inicializarEstado();
 	}
 	
@@ -31,43 +33,9 @@ public class ProcesoSobreLocalComercial extends ProcesoSobreLocalComercialClaseA
 
 	public void actualizarLocalComercial() {
 		archivo = new File(this.getRuta());
-		FileReader lector = null;
-		String linea;
 		this.seActualizaronLosLocalesComerciales();
 		try {
-			lector = new FileReader(archivo);
-			BufferedReader br = new BufferedReader(lector);
-			while ((linea = br.readLine()) != null) {
-				int i = 0;
-				ArrayList<String> palabrasClaves = new ArrayList<>();
-				char c = linea.charAt(i);
-				StringBuilder constructorNombre = new StringBuilder();
-				StringBuilder constructorPalabra = new StringBuilder();
-				while (c != ';') {
-					constructorNombre.append(c);
-					i++;
-				}
-				String nombreDelLocal = constructorNombre.toString();
-				List<PuntoDeInteres> puntosQueMatchean = getUnMapa().buscoEnElSistema(nombreDelLocal, null);
-				if (!puntosQueMatchean.isEmpty()) {
-					while (c != '\0') {
-						while (c != ' ') {
-							if (c == '\0')
-								break;
-							constructorPalabra.append(c);
-							i++;
-						}
-						palabrasClaves.add(constructorPalabra.toString());
-						if (c == '\0')
-							break;
-						i++;
-					}
-					
-					puntosQueMatchean.stream().forEach(poi->poi.agregarPalabrasClaves(palabrasClaves));
-					this.setCantidadDeElementosAfectados(puntosQueMatchean.size());
-				}
-				br.close();
-			}
+			traductor.leerArchivo(archivo, this);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/main/java/F5/lectorArchivoLocalComercial.java
@@ -0,0 +1,49 @@
+package F5;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class lectorArchivoLocalComercial {
+
+	public void leerArchivo(File archivo, ProcesoSobreLocalComercial unLocal){
+		FileReader lector = null;
+		String linea;
+		lector = new FileReader(archivo);
+		BufferedReader br = new BufferedReader(lector);
+		while ((linea = br.readLine()) != null) {
+			int i = 0;
+			ArrayList<String> palabrasClaves = new ArrayList<>();
+			char c = linea.charAt(i);
+			StringBuilder constructorNombre = new StringBuilder();
+			StringBuilder constructorPalabra = new StringBuilder();
+			while (c != ';') {
+				constructorNombre.append(c);
+				i++;
+			}
+			String nombreDelLocal = constructorNombre.toString();
+			List<PuntoDeInteres> puntosQueMatchean = unLocal.getUnMapa().buscoEnElSistema(nombreDelLocal, null);
+			if (!puntosQueMatchean.isEmpty()) {
+				while (c != '\0') {
+					while (c != ' ') {
+						if (c == '\0')
+							break;
+						constructorPalabra.append(c);
+						i++;
+					}
+					palabrasClaves.add(constructorPalabra.toString());
+					if (c == '\0')
+						break;
+					i++;
+				}
+				
+				puntosQueMatchean.stream().forEach(poi->poi.agregarPalabrasClaves(palabrasClaves));
+				unLocal.setCantidadDeElementosAfectados(puntosQueMatchean.size());
+			}
+			br.close();
+		
+	}
+	
+}
