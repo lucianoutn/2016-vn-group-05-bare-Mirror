40f5aac44eb40e0c974e5e22e7af3bc9e42a4ceb
Luciano Marques <luciano.utn@gmail.com>
2016-07-27 16:30:37 -0300

    arreglo el mock del notificador de admin

--- src/test/java/F5/NotificadorDeAdministradorMock.java
+++ src/test/java/F5/NotificadorDeAdministradorMock.java
@@ -1,23 +1,31 @@
 package F5;
 
-
-
 public class NotificadorDeAdministradorMock implements NotificadorDeBusqueda {
 
 	// atributos
+	public int tiempoParaNotificar = 10; // debe ser parametrizable
 	public boolean notificado = false;
-	
+
 	// métodos
-	
+
 	public void notificarBusqueda(Busqueda unaBusqueda) {
 
-		if (unaBusqueda.excedioDemora()) {
-			//no hago nada xq soy un mock. cambio la siguiente variable para tests
+		if (this.excedioDemora(unaBusqueda)) {
+			// no hago nada xq soy un mock. cambio la siguiente variable para
+			// tests
 			this.notificado = true;
-			
+
 		}
 
 	}
 
-	
+	public void setTiempoParaNotificar(int tiempoParaNotificar) {
+		// debe ser parametrizable
+		this.tiempoParaNotificar = tiempoParaNotificar; // en segundos
+	}
+
+	public boolean excedioDemora(Busqueda unaBusqueda) {
+		return unaBusqueda.tiempoBusqueda >= this.tiempoParaNotificar;
+	}
+
 }
