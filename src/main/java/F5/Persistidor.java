83f29c9cc350e5c03911e57aff0870b25e302bcd
Luciano Marques <luciano.utn@gmail.com>
2016-10-13 05:01:10 -0300

    hago singleton al persistidor

--- src/main/java/F5/Persistidor.java
+++ src/main/java/F5/Persistidor.java
@@ -1,17 +1,22 @@
 package F5;
 
-
-
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
 import F5.Pois.PuntoDeInteres;
 
-public class Persistidor extends AbstractPersistenceTest implements WithGlobalEntityManager{
+public class Persistidor extends AbstractPersistenceTest implements WithGlobalEntityManager {
 
-	public void persistirUnElemento(Object unObjeto){
-			entityManager().persist(unObjeto);
+	private final static Persistidor instancia = new Persistidor();
+
+	
+	public static Persistidor getInstancia(){
+		
+		return instancia;
 	}
 	
-	
+	public void persistirUnElemento(Object unObjeto) {
+		entityManager().persist(unObjeto);
+	}
+
 }
