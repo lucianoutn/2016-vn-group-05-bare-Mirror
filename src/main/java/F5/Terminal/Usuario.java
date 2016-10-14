e05a664a2f06039788e894e9768c2687e6bf8492
Luciano Marques <luciano.utn@gmail.com>
2016-10-14 02:13:46 -0300

    persistidor de usuarios completo

--- src/main/java/F5/Terminal/Usuario.java
+++ src/main/java/F5/Terminal/Usuario.java
@@ -5,21 +5,22 @@ import java.util.List;
 
 import javax.persistence.*;
 
-import javax.persistence.Id;
-
 import F5.AccionPostBusqueda;
+import F5.Persistencia.PersistidorDePOIs;
+import F5.Persistencia.PersistidorDeUsuarios;
 import F5.Pois.Comuna;
 
 @Entity
+@Table(name = "Usuarios")
 public class Usuario {
 	@Id
 	@GeneratedValue
 	private Integer id;
-	
+
 	private String nombre;
 	@ManyToOne
 	private Comuna comuna;
-	
+
 	public Comuna getComuna() {
 		return comuna;
 	}
@@ -27,38 +28,40 @@ public class Usuario {
 	public String getNombre() {
 		return nombre;
 	}
+
 	@Transient
 	private List<AccionPostBusqueda> accionesRealizables = new ArrayList<AccionPostBusqueda>();
-	
+
 	public List<AccionPostBusqueda> getAccionesRealizables() {
 		return accionesRealizables;
 	}
 
-	public Usuario(String unNombre, Comuna unaComuna){
-		nombre=unNombre;
+	public Usuario(String unNombre, Comuna unaComuna) {
+		nombre = unNombre;
 		comuna = unaComuna;
+		PersistidorDeUsuarios.getInstancia().guardaParaPersistir(this);
 	}
-	
-	public Usuario(){
-		
+
+	public Usuario() {
+
 	}
 
 	public void ejecutarAcciones() {
-		accionesRealizables.forEach(accion-> accion.ejecutar());
-		
+		accionesRealizables.forEach(accion -> accion.ejecutar());
+
 	}
 
 	public void agregarAcciones(List<AccionPostBusqueda> acciones) {
-		if(acciones!=null)
-			accionesRealizables.addAll(acciones);		
+		if (acciones != null)
+			accionesRealizables.addAll(acciones);
 	}
-	
-	public void agregarAccion(AccionPostBusqueda unaAccion){
+
+	public void agregarAccion(AccionPostBusqueda unaAccion) {
 		accionesRealizables.add(unaAccion);
 	}
 
 	public void quitarAcciones(List<AccionPostBusqueda> acciones) {
-		if(acciones!=null)
+		if (acciones != null)
 			accionesRealizables.removeAll(acciones);
 	}
 }
