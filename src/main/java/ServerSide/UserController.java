4bec46de271c91a37ff959e18c55be8723cd64e2
HaydossianEzequiel <ezequiel.haydossian@mercadolibre.com>
2016-10-31 22:59:24 -0300

    puedo mostrar una lista de pois

--- src/main/java/ServerSide/UserController.java
+++ src/main/java/ServerSide/UserController.java
@@ -1,11 +1,16 @@
 package ServerSide;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
+import F5.Pois.PuntoDeInteres;
+import F5.Pois.SucursalDeBanco;
+import TestFactory.BancoFactory;
 import spark.*;
 
 public class UserController implements WithGlobalEntityManager, TransactionalOps{
@@ -15,8 +20,15 @@ public class UserController implements WithGlobalEntityManager, TransactionalOps
 	public ModelAndView login(Request req, Response res) throws Exception{
 		
 		
-		Map<String, String> model = new HashMap<>();
-		model.put("nombre", "Ezequiel");
+		Map<String, List<Object>> model = new HashMap<>();
+		
+		//esto tiene qe salir del bootstrap cuando almacena en la base
+		List<Object> pois = new ArrayList<Object>(); 
+		pois.add(new SucursalDeBanco("HSBC", null, null));
+		pois.add(new SucursalDeBanco("galicia", null, null));
+		pois.add(new SucursalDeBanco("frances", null, null));
+		
+		model.put("bancos", pois);
 		
 		return new ModelAndView(model, "user-login.hbs");
 	}
