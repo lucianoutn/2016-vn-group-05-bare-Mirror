b44d6be6932a155b4556bc5c39f213c7b92987b1
Luciano Marques <luciano.utn@gmail.com>
2016-11-05 03:45:14 -0300

    redirecciono la ruta para el landing

--- src/main/java/ServerSide/Router.java
+++ src/main/java/ServerSide/Router.java
@@ -3,30 +3,32 @@ package ServerSide;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.utils.*;
 import spark.Spark;
+
+import com.sun.mail.iap.Response;
+
 import Spark.*;
 
-
 public class Router {
-	
+
 	public static void configure() {
-		HandlebarsTemplateEngine engine = HandlebarsTemplateEngineBuilder
-				.create()
-				.withDefaultHelpers()
-				.withHelper("isTrue", BooleanHelper.isTrue)
-				.build();
-	
+		HandlebarsTemplateEngine engine = HandlebarsTemplateEngineBuilder.create().withDefaultHelpers()
+				.withHelper("isTrue", BooleanHelper.isTrue).build();
+
 		Spark.staticFiles.location("/public");
-		
+
 		LoginController loginController = new LoginController();
 		AdministradorController administradorController = new AdministradorController();
 		TerminalController terminalController = new TerminalController();
-		
+
+		Spark.get("/", (request, response) -> {
+			response.redirect("/user/login");
+			return null;
+		});
+		Spark.get("/user/login", loginController::login, engine);
 		Spark.get("/user/login", loginController::login, engine);
 		Spark.get("/user/checkLogin", loginController::checkLogin, engine);
 		Spark.get("/terminal/show", terminalController::terminalShow, engine);
 		Spark.get("/administrador/pois/show", administradorController::administradorPoisShow, engine);
-		
-		
-		
+
 	}
 }
